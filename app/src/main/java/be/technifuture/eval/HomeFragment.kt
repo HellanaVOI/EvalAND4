9c9c6cb61a723cbea16ddcb818c09a5d47b977d6
Tony <izzi.tony@gmail.com>
2023-09-08 12:07:17 +0200

    Commit avant de manger :D

--- app/src/main/java/be/technifuture/eval/HomeFragment.kt
+++ app/src/main/java/be/technifuture/eval/HomeFragment.kt
@@ -7,16 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import be.technifuture.eval.databinding.FragmentHomeBinding
 import be.technifuture.eval.model.Expense
 import be.technifuture.eval.viewModel.ExpenseAdapter
 import be.technifuture.eval.viewModel.ExpenseListViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -30,24 +26,26 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        binding.addButton.setOnClickListener{
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToAddFragment())
+        }
+
         return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        expListViewModel = ViewModelProvider(this).get(ExpenseListViewModel::class.java)
+        expListViewModel = ViewModelProvider(this)[ExpenseListViewModel::class.java]
         expListViewModel.getBooks(requireContext()).observeForever {
             updateData(it)
         }
 
-        /*binding.addButton.setOnClickListener{
-            val direction = HomeFragmentDirections.actionHomeFragmentToAddFragment()
-            findNavController().navigate(direction)
-        }*/
+
     }
 
-    fun updateData(expense: List<Expense>) {
+    private fun updateData(expense: List<Expense>) {
         adapter = ExpenseAdapter(expense.toMutableList())
         binding.recycleView.layoutManager = LinearLayoutManager(
             context,
